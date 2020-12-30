502d10cd7323e96dfb32c1693ffb3b24e43e6b7c
Jonathan Narvaez <jonaser06@gmail.com>
2020-12-29 23:20:58 -0500

    fix

--- src/app/pages/stadistics/stadistics.page.ts
+++ src/app/pages/stadistics/stadistics.page.ts
@@ -7,9 +7,81 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StadisticsPage implements OnInit {
 
-  constructor() { }
+  dialogNewStat: boolean = false;
+  dialogRemove: boolean = false;
+  dialogBulletin: boolean = false;
+  dialogIndicator: boolean = false;
+  titleDialog: string = "";
+  subtitleDialog: string = "";
+  titleDialogRemove: string ="";
+  isEdited = false;
+
+  constructor() {
+    this.dialogNewStat = false;
+    this.dialogRemove = false;
+    this.dialogBulletin = false;
+  }
 
   ngOnInit() {
   }
 
+  //Estadisticas
+  openDialogStat() {
+    this.dialogNewStat = true;
+  }
+  closeDialogStat() {
+    this.dialogNewStat = false;
+    this.isEdited = false;
+  }
+  editStat(item){
+    this.isEdited = true;
+    this.openDialogStat();
+  }
+  removeStat(item){
+    this.openDialogRemove();
+    this.titleDialogRemove = "ELIMINAR GRÁFICO";
+  }
+
+  // Boletin
+  openDialogBulletin() {
+    this.dialogBulletin = true;
+  }
+  closeDialogBulletin() {
+    this.dialogBulletin = false;
+    this.isEdited = false;
+  }
+  editBulletin(item){
+    this.isEdited = true;
+    this.openDialogBulletin();
+  }
+  removeBulletin(item){
+    this.openDialogRemove();
+    this.titleDialogRemove = "ELIMINAR BOLETÍN";
+  }
+
+  //Indicadores
+  openDialogIndicator() {
+    this.dialogIndicator = true;
+  }
+  closeDialogIndicator() {
+    this.dialogIndicator = false;
+    this.isEdited = false;
+  }
+  editIndicator(item){
+    this.isEdited = true;
+    this.openDialogIndicator();
+  }
+  removeIndicator(){
+    this.openDialogRemove();
+    this.titleDialogRemove = "ELIMINAR INDICADOR";
+  }
+
+  //Dialogo eliminar
+  openDialogRemove() {
+    this.dialogRemove = true;
+  }
+  closeDialogRemove() {
+    this.dialogRemove = false;
+  }
+  
 }
