7269a72a229a5bd7c4efd3bb826c8bb81c88f968
Jonathan Narvaez <jonaser06@gmail.com>
2021-01-16 01:51:17 -0500

    fix

--- src/app/pages/infcenter/news/news.page.ts
+++ src/app/pages/infcenter/news/news.page.ts
@@ -33,15 +33,19 @@ export class NewsPage implements OnInit {
     this.dialogReadNews = false;
     this.dialogCreateNews =false;
   }
+
   getnoticia(){
     this.infcenterService.get_infcenter()
     .then(resp=>{
       // console.log(resp);
       this.NewsData = resp['data'];
-      console.log(this.NewsData);
     })
     .catch();
     
+  }
+
+  openNew_(ID_NO){
+    console.log(ID_NO);
   }
 
   openDialogInfo(){
