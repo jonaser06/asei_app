508d5b24f27db6c122b21be0304678401f0ec820
jose ms <josehsuarez20@gmail.com>
2021-01-16 02:11:19 -0500

    pages1

--- src/app/pages/infcenter/news/news.page.ts
+++ src/app/pages/infcenter/news/news.page.ts
@@ -16,23 +16,8 @@ export class NewsPage implements OnInit {
   dialogReadNews: boolean = false;
   dialogCreateNews: boolean = false;
 
-<<<<<<< HEAD
-  /* formulario de noticias */
-
-  ID_NO : any;
-  titulo : any;
-  resumen : any;
-  texto : any;
-  fecha_inicio : any;
-  fecha_fin : any;
-  seccion : any;
-  files : any;
-
-
-=======
   /* noticias */
   NewsData : any;
->>>>>>> b66a085f4fc0f8ed1754e0197bb5ca3958ed4d17
   
   constructor(private redireccionService: RedireccionService,private infcenterService: InfcenterService) { 
     this.getnoticia();
@@ -58,71 +43,19 @@ export class NewsPage implements OnInit {
 
   getnoticia(){
     this.infcenterService.get_infcenter()
-<<<<<<< HEAD
-    .then(rspt=>{
-      console.log(rspt);
-=======
     .then(resp=>{
       // console.log(resp);
       this.NewsData = resp['data'];
->>>>>>> b66a085f4fc0f8ed1754e0197bb5ca3958ed4d17
     })
     .catch();
     
   }
   
-<<<<<<< HEAD
-  // editnews(objnews){
-    
-  //   this.ID_NOnews = objnews.ID_NO;
-  //   this.titulonews = objnews.titulo;
-  //   this.resumennews = objnews.resumen;
-  //   this.textonews = objnews.texto;
-  //   this.fecha_inicionews = objnews.fecha_inicio;
-  //   this.fecha_finnews = objnews.fecha_fin;
-  //   this.seccionnews = objnews.seccion;
-  //   this.filesnews = objnews.files;
-
-  //   this.isEdited = true;
-  //   this.openDialogNews();
-
-  // }
-
-  // removenews(item){
-  //   this.openDialogRemove();
-  //   this.titleDialogRemove = "ELIMINAR BOLETÍN";
-  //   this.bull_rmv = true;
-  // }
-
-  // load_news(){
-  //   this.years = [];
-  //   this.bulletinService.get_bulletin()
-  //   .then(resp=>{
-  //     resp['data'].forEach( (data, index) =>{
-  //       data.file = environment.url + '/' + data.file;
-  //       console.log(index);
-  //       this.years.push(data.year);
-  //     });
-  //     this.years = [...new Set(this.years)];
-  //     this.bulletin_data = resp['data'];
-  //   })
-  //   .catch(err=>{
-  //     console.log(err);
-  //   });
-  // }
-
-  // bulletedit(fbullet: NgForm){
-
-  // }
-  // bullet(fbullet: NgForm){
-=======
   openNew_(ID_NO){
     console.log(ID_NO);
   }
 
-  openDialogInfo(){
->>>>>>> b66a085f4fc0f8ed1754e0197bb5ca3958ed4d17
+  openDialogInfo(){}
     
-  // }
-
+ 
 }
