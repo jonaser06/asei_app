9a81207f2fbf9c18321492fcb97ddd94eeaba2fc
jose ms <josehsuarez20@gmail.com>
2021-01-17 17:39:04 -0500

    update pages

--- src/app/pages/infcenter/fairs/fairs.page.ts
+++ src/app/pages/infcenter/fairs/fairs.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { InfcenterService } from 'src/app/services/infcenter.service';
+import { RedireccionService } from '../../../services/redireccion.service';
 
 @Component({
   selector: 'app-fairs',
@@ -10,7 +11,7 @@ export class FairsPage implements OnInit {
 
   dialogFeriasRead: boolean = false;
   dialogFeriasCreate: boolean = false;
-  constructor(private infcenterService: InfcenterService) { 
+  constructor(private infcenterService: InfcenterService, private redireccionService: RedireccionService) { 
     this.getferias();
   }
 
@@ -18,17 +19,21 @@ export class FairsPage implements OnInit {
   }
 
   createDialogFerias(){
-    this.dialogFeriasCreate = true;
+    // this.dialogFeriasCreate = true;
+    this.redireccionService.redireccion('tabs/infcenter/fairs/create')
   }
 
   readDialogFerias(){
-    this.dialogFeriasRead = true;
+    // this.dialogFeriasRead = true;
+    this.redireccionService.redireccion('tabs/infcenter/fairs/info')
   }
 
-  closeDialogInfo(){
-    this.dialogFeriasCreate = false;
-    this.dialogFeriasRead =false;
-  }
+  // closeDialogInfo(){
+  //   this.dialogFeriasCreate = false;
+  //   this.dialogFeriasRead =false;
+  // }
+
+  
 
   getferias(){
     this.infcenterService.get_infcenterFerias()
