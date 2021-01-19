f03cd23d51971885a98be19b03968d11c05321de
Jonathan Narvaez <jonaser06@gmail.com>
2021-01-18 23:45:05 -0500

    fix

--- src/app/pages/infcenter/news/edit/edit.page.ts
+++ src/app/pages/infcenter/news/edit/edit.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { UiServiceService } from 'src/app/services/ui-service.service';
+import { RedireccionService } from '../../../../services/redireccion.service';
+import { InfcenterService } from '../../../../services/infcenter.service';
 
 @Component({
   selector: 'app-edit',
@@ -7,9 +11,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditPage implements OnInit {
 
-  constructor() { }
+  /* editar noticia */
+  titulo : any;
+  resumen : any;
+  texto : any;
+  seccion : any;
+  fecha_publicacion : any;
+  fileToUploadstat: any;
+  imagestat: any;
+
+  constructor( private redireccionService: RedireccionService, private uiserviceService: UiServiceService , private infcenterService: InfcenterService ) { }
 
   ngOnInit() {
+  }
+
+  handleFileInput(event){
+    this.fileToUploadstat = event.target.files[0];
+    const reader = new FileReader();
+    reader.readAsDataURL(this.fileToUploadstat);
+    reader.onload = () => {
+        this.imagestat = reader.result;
+    };
+  }
+
+  volvernews(){
+    // this.dialogCreateNews= true;
+    this.redireccionService.backpage();
+  }
+
+  editNews(fNews: NgForm){
+    if(!this.fileToUploadstat) return this.uiserviceService.alert_info('selecciona una imagen');
+    if(!this.titulo) return this.uiserviceService.alert_info('Es necesario un titulo');
+    if(!this.resumen) return this.uiserviceService.alert_info('Es necesario el resumen');
+    if(!this.texto) return this.uiserviceService.alert_info('Es necesario el texto');
+    // if(!this.seccion) return this.uiserviceService.alert_info('Es necesario la descripcion');
+    if(!this.fecha_publicacion) return this.uiserviceService.alert_info('Es necesario la fecha de publicacion');
+
+    let formdata = new FormData;
+    formdata.append('titulo', this.titulo);
+    formdata.append('resumen', this.resumen);
+    formdata.append('texto', this.texto);
+    formdata.append('fecha_publicacion', this.fecha_publicacion);
+    formdata.append('seccion', 'noticias');
+    formdata.append("files[]", this.fileToUploadstat);
+
+    this.infcenterService.create_infcenterNews(formdata)
+    .then(resp=>{ 
+      console.log('SE CREO LA NOTA');
+      this.redireccionService.backpage();
+    })
+    .catch();
+
   }
 
 }
