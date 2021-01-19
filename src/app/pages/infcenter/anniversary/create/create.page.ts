9f983ff2d0656f8dc14c4a8108dea37a8ff9dd49
jose ms <josehsuarez20@gmail.com>
2021-01-19 02:56:25 -0500

    update

--- src/app/pages/infcenter/anniversary/create/create.page.ts
+++ src/app/pages/infcenter/anniversary/create/create.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { UiServiceService } from 'src/app/services/ui-service.service';
 import { RedireccionService } from '../../../../services/redireccion.service';
+import { InfcenterService } from '../../../../services/infcenter.service';
 
 @Component({
   selector: 'app-create',
@@ -8,14 +11,58 @@ import { RedireccionService } from '../../../../services/redireccion.service';
 })
 export class CreatePage implements OnInit {
 
-  constructor(private redireccionService: RedireccionService) { }
+  /* nueva Aniversario */
+  titulo : any;
+  resumen : any;
+  texto : any;
+  // seccion : any;
+  fecha_publicacion : any;
+  hora_publicacion : any;
+  fileToUploadstat: any;
+  imagestat: any;
+
+  constructor(private redireccionService: RedireccionService, private uiserviceService: UiServiceService, private infcenterService: InfcenterService) { }
 
   ngOnInit() {
   }
-
+  handleFileInput(event){
+    this.fileToUploadstat = event.target.files[0];
+    const reader = new FileReader();
+    reader.readAsDataURL(this.fileToUploadstat);
+    reader.onload = () => {
+        this.imagestat = reader.result;
+    };
+  }
   volverAnniversary(){
     // this.dialogCreateNews= true;
     this.redireccionService.backpage();
+  }
+
+  createAniversario(fAniversario: NgForm){
+    if(!this.fileToUploadstat) return this.uiserviceService.alert_info('selecciona una imagen');
+    if(!this.titulo) return this.uiserviceService.alert_info('Es necesario un titulo');
+    if(!this.resumen) return this.uiserviceService.alert_info('Es necesario el resumen');
+    if(!this.texto) return this.uiserviceService.alert_info('Es necesario el texto');
+    // if(!this.seccion) return this.uiserviceService.alert_info('Es necesario la descripcion');
+    if(!this.fecha_publicacion) return this.uiserviceService.alert_info('Es necesario la fecha de publicacion');
+    if(!this.hora_publicacion) return this.uiserviceService.alert_info('Es necesario la hora de publicacion');
+
+    let formdata = new FormData;
+    formdata.append('titulo', this.titulo);
+    formdata.append('resumen', this.resumen);
+    formdata.append('texto', this.texto);
+    formdata.append('fecha_publicacion', this.fecha_publicacion);
+    formdata.append('fecha_publicacion', this.hora_publicacion);
+    formdata.append('seccion', 'noticias');
+    formdata.append("files[]", this.fileToUploadstat);
+
+    this.infcenterService.create_infcenterNews(formdata)
+    .then(resp=>{ 
+      console.log('SE CREO EL ANIVERSARIO');
+      this.redireccionService.backpage();
+    })
+    .catch();
+
   }
 
 }
--- src/app/pages/infcenter/eventos/create/create.page.ts
+++ src/app/pages/infcenter/eventos/create/create.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { RedireccionService } from '../../../../services/redireccion.service';
+import { InfcenterService } from '../../../../services/infcenter.service';
+import { UiServiceService } from '../../../../services/ui-service.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-create',
@@ -8,7 +11,19 @@ import { RedireccionService } from '../../../../services/redireccion.service';
 })
 export class CreatePage implements OnInit {
 
-  constructor(private redireccionService: RedireccionService) { }
+    /* nueva eventos */
+    titulo : any;
+    resumen : any;
+    texto : any;
+    // seccion : any;
+    fecha_inicio : any;
+    fecha_fin : any;
+    hora_inicio : any;
+    hora_fin : any;
+    fileToUploadstat: any;
+    imagestat: any;
+
+  constructor(private redireccionService: RedireccionService, private infcenterService: InfcenterService, private uiserviceService: UiServiceService) { }
 
   ngOnInit() {
   }
@@ -19,9 +34,45 @@ export class CreatePage implements OnInit {
     this.redireccionService.backpage();
   }
   
+  handleFileInput(event){
+    this.fileToUploadstat = event.target.files[0];
+    const reader = new FileReader();
+    reader.readAsDataURL(this.fileToUploadstat);
+    reader.onload = () => {
+        this.imagestat = reader.result;
+    };
+  }
+  createEventos(fEventos: NgForm){
+    if(!this.fileToUploadstat) return this.uiserviceService.alert_info('selecciona una imagen');
+    if(!this.titulo) return this.uiserviceService.alert_info('Es necesario un titulo');
+    if(!this.resumen) return this.uiserviceService.alert_info('Es necesario el resumen');
+    if(!this.texto) return this.uiserviceService.alert_info('Es necesario el texto');
+    // if(!this.seccion) return this.uiserviceService.alert_info('Es necesario la descripcion');
+    if(!this.fecha_inicio) return this.uiserviceService.alert_info('Es necesario la fecha de inicio');
+    if(!this.fecha_fin) return this.uiserviceService.alert_info('Es necesario la fecha fin');
+    if(!this.hora_inicio) return this.uiserviceService.alert_info('Es necesario la hora de inicio');
+    if(!this.hora_fin) return this.uiserviceService.alert_info('Es necesario la hora fin');
+
+    let formdata = new FormData;
+    formdata.append('titulo', this.titulo);
+    formdata.append('resumen', this.resumen);
+    formdata.append('texto', this.texto);
+    formdata.append('fecha_publicacion', this.fecha_inicio);
+    formdata.append('fecha_publicacion', this.fecha_fin);
+    formdata.append('fecha_publicacion', this.hora_inicio);
+    formdata.append('fecha_publicacion', this.hora_fin);
+    formdata.append('seccion', 'noticias');
+    formdata.append("files[]", this.fileToUploadstat);
+
+    this.infcenterService.create_infcenterNews(formdata)
+    .then(resp=>{ 
+      console.log('SE CREO EL EVENTO');
+      this.redireccionService.backpage();
+    })
+    .catch();
+
+  }
 
 }
 
-16450493
 
-33138731
