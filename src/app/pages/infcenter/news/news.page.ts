3c61ebf1b33eea8b3da84d700a41ad4ca741b940
Jonathan Narvaez <jonaser06@gmail.com>
2021-01-25 01:02:24 -0500

    fix

--- src/app/pages/infcenter/news/news.page.ts
+++ src/app/pages/infcenter/news/news.page.ts
@@ -130,6 +130,7 @@ export class NewsPage implements OnInit {
   search_(buscatxt){
     let pages = [];
     this.currentkey = buscatxt;
+    this.currentkeyOld = buscatxt;
     this.infcenterService.search_infcenter('noticias', 1, buscatxt)
     .then(resp=>{ 
       console.log('Buscar nota : '+buscatxt);
