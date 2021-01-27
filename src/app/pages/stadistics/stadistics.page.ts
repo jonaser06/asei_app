23a179d87d8d45f441866c5aa895842b37658905
Jonathan Narvaez <jonaser06@gmail.com>
2021-01-27 00:36:21 -0500

    fix

--- src/app/pages/stadistics/stadistics.page.ts
+++ src/app/pages/stadistics/stadistics.page.ts
@@ -83,6 +83,51 @@ export class StadisticsPage implements OnInit {
     this.current_rol();
     console.log('constructor');
   }
+  
+  slideConfig = {"slidesToScroll": 4,"slidesToShow": 4,"margin":"25px","dots":true,
+  responsive: [
+    {
+      breakpoint: 1600,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll:4
+      }
+    },
+    {
+      breakpoint: 1260,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll:4
+      }
+    },
+    {
+      breakpoint: 1000,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll:3
+      }
+    },
+    {
+      breakpoint: 800,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll:2
+      }
+    },
+    {
+      breakpoint: 500,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll:1
+      }
+    },
+    {
+      breakpoint: 0,
+      settings: {
+        slidesToShow: 1
+      }
+    }
+  ]};
 
   ngOnInit() { }
 
