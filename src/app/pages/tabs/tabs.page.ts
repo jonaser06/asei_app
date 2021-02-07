3777b5b47775deebf0c8e4069d9d97c970feb75e
Jonathan Narvaez <jonaser06@gmail.com>
2021-02-06 22:55:05 -0500

    fix

--- src/app/pages/tabs/tabs.page.ts
+++ src/app/pages/tabs/tabs.page.ts
@@ -29,8 +29,8 @@ export class TabsPage implements OnInit {
     });
   }
   setCurrentActive(path: string){
-    console.log (path);
-    return;
+    // console.log (path);
+    // return;
     this.navCtrol.navigateRoot(`/tabs/${path}`);
   }
   goProfile(){
