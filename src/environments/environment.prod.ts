bf5c87b2302d71718b5d60e895efbf21ab8f3853
Jonathan Narvaez <jonaser06@gmail.com>
2021-05-21 18:41:50 -0500

    fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  url: 'http://ec2-54-221-209-57.compute-1.amazonaws.com',
+  url: 'https://api.asei.online',
   // url: 'http://localhost/asei_api',
 };
